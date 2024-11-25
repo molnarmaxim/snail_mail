598ec4e03fbbd7520f0e0d1112c2316524068241
molnarmaxim <molnar.maxim@diak.szbi-pg.hu>
2024-11-25 09:25:53 +0100

    Feladat elkészítve

--- snail_mail.py
+++ snail_mail.py
@@ -1,4 +1,4 @@
-email = input("Your email address: ")
+#email = input("Your email address: ")
 
 # "hello.worldcom"    => An email address has to contain a '@' character!
 # "he@llo@world.com"  => An email address cannot contain more than one '@' characters!
@@ -13,26 +13,54 @@ email = input("Your email address: ")
 # "hello@world.com"   => Valid email address :)
 
 
-length_of_email = len(email)
-number_of_at_characters = email.count("@")
-number_of_dot_characters = email.count(".")
-position_of_at = email.find("@")
-
-position_of_first_dot = email.find(".")
-position_of_last_dot = email.rfind(".")
-position_of_first_dot_after_the_at = email.find(".", position_of_at)
 
 
-error_message_no_at = "An email address has to contain a '@' character!"
-error_message_too_many_at = "An email address cannot contain more than one '@' characters!"
-error_message_no_dot = "An email address has to contain at least one '.' character!"
-error_message_no_username = "The username before the '@' character cannot be empty!"
-error_message_no_dot_in_domain = "The domain has to contain at least one '.' character!"
-error_message_no_server_name = "The domain cannot start with a '.' character!"
-error_message_no_tld = "The top-level domain cannot be empty!"
-error_message_short_tld = "The top-level domain has to be at least two characters long!"
-error_message_no_domain = "The domain after the '@' character cannot be empty!"
-error_message_invalid_username = "The username cannot start with a '.' character!"
 
+error_message_no_at = "An email address has to contain a '@' character!" #kesz
+error_message_too_many_at = "An email address cannot contain more than one '@' characters!" #kesz
+error_message_no_dot = "An email address has to contain at least one '.' character!" #kesz
+error_message_no_username = "The username before the '@' character cannot be empty!" #kesz
+error_message_no_dot_in_domain = "The domain has to contain at least one '.' character!" #kesz
+error_message_no_server_name = "The domain cannot start with a '.' character!" #kesz
+error_message_no_tld = "The top-level domain cannot be empty!" #kesz
+error_message_short_tld = "The top-level domain has to be at least two characters long!" #kesz
+error_message_no_domain = "The domain after the '@' character cannot be empty!" #kesz
+error_message_invalid_username = "The username cannot start with a '.' character!" #kesz
 ok_message = "Valid email address :)"
-is_valid = True
+is_valid = False
+
+while not is_valid:
+    email = input("Add meg az e-mail címed: ")
+    length_of_email = len(email)
+    number_of_at_characters = email.count("@")
+    number_of_dot_characters = email.count(".")
+    position_of_at = email.find("@")
+
+    position_of_first_dot = email.find(".")
+    position_of_last_dot = email.rfind(".")
+    position_of_first_dot_after_the_at = email.find(".", position_of_at)
+
+    if "@" not in email:
+        print(error_message_no_at)
+    elif email.count("@") > 1:
+        print(error_message_too_many_at)
+    elif email.endswith("@"):
+        print(error_message_no_domain)
+    elif "." not in email:
+        print(error_message_no_dot)
+    elif email.startswith("@"):
+        print(error_message_no_username)
+
+    elif position_of_first_dot_after_the_at < 1:
+        print(error_message_no_dot_in_domain)
+    elif position_of_first_dot_after_the_at == position_of_at + 1:
+        print(error_message_no_server_name)
+    elif email.endswith("."):
+        print(error_message_no_tld)
+    elif len(email.split(".")[-1]) < 2:
+        print(error_message_short_tld)
+    elif email.startswith("."):
+        print(error_message_invalid_username)
+    else:
+        print(ok_message)
+        is_valid = True
